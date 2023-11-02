0d901ccaac165187de40570726a9e7a98566641f
Swichu553 <swicha@o2.pl>
2023-11-02 21:38:35 +0100

    add loginForm and tokken to cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 import { LoginForm } from './components/LoginForm/LoginForm';
+import { apiUrl } from './config/api';
 
 export const App = () => {
-  const handleLogin = (username: string, password: string) => {
+  const handleLogin = async (username: string, password: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    console.log(`Logging in with username: ${username} and password: ${password}`);
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+
+        // tokken umieszczony w cookie
+        document.cookie = `token=${token}; max-age=900`; // Token ważny przez 15 minut
+
+      } else {
+        console.error('Login failed.');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
   }
+
   return (
     <>
       <LoginForm onLogin={handleLogin} />
