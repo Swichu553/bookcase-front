14ef70cb185982d12faf67cc317c9f9ab4313d32
Swichu553 <swicha@o2.pl>
2023-11-04 15:21:45 +0100

    change app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,43 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { LoginForm } from './components/LoginForm/LoginForm';
 import { MyBooks } from './components/MyBooks/MyBooks';
+import { Header } from './components/Header/Header';
 import { apiUrl } from './config/api';
 import './App.css';
-import { Header } from './components/Header/Header';
+//import Cookies from 'js-cookie';
 
 export const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+  //const token = Cookies.get('token');
   const handleLogin = async (username: string, password: string) => {
-    try {
-      const response = await fetch(`${apiUrl}/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+    if (false) {
+      setIsAuthenticated(true);
+    } else {
+      try {
+        const response = await fetch(`${apiUrl}/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
 
-      if (response.ok) {
-        const data = await response.json();
-        const token = data.token;
-        setIsAuthenticated(true);
-        // Token umieszczony w ciasteczku
-        document.cookie = `token=${token}; max-age=900`; // Token ważny przez 15 minut
-      } else {
-        console.error('Login failed.');
+        if (response.ok) {
+          const data = await response.json();
+          const token = data.token;
+          setIsAuthenticated(true);
+          // Token umieszczony w ciasteczku
+          //  Cookies.set('token', token, { expires: 15 / (60 * 24) }); // 15 minut
+
+        } else {
+          console.error('Login failed.');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
       }
-    } catch (error) {
-      console.error('An error occurred:', error);
     }
   }
+
 
   return (
     <div>
